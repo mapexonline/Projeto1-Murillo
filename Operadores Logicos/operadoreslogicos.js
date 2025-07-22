fe9c03c9063ef15380d8befc88b7a2dd1ff19020
mapexonline <mapexonline22@gmail.com>
2025-07-22 18:21:56 -0300

    Add files via upload

---
+++ Operadores Logicos/operadoreslogicos.js
@@ -0,0 +1,16 @@
+//Operadores Logicos
+//&& (And) - Duas informaçoes verdadeiras
+
+let idade = 20;
+let codigoPromocional = true;
+
+//Verificação dos valores
+let verficação = idade >= 18 && codigoPromocional;
+console.log(verficação);
+
+//|| (or) - só uma das informações tem que ser verdadeira
+
+let verficação2 = idade >= 21 || codigoPromocional;
+console.log(verficação2);
+
+//! (not) - ele verfica os valores booleanos e os inverte
