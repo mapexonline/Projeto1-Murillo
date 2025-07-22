fe15474a77729df5adc8dcce1704842e2383f8ef
mapexonline <mapexonline22@gmail.com>
2025-07-22 18:22:54 -0300

    Add files via upload

---
+++ Operadores de Comparação/OpdeComparação.js
@@ -0,0 +1,18 @@
+//Operaçoes de comparação
+
+//Igualdade
+console.log(5 == "5"); //Não Indicado
+console.log(5 === "5"); //Indicado
+
+//Estrita igualdade
+console.log(5 != "5"); //Não indicado
+console.log(5 !== "5"); //Indicado
+
+//Maior que, Menor que, Maior ou igual, Menor ou igual
+console.log(5 > "5");
+console.log(5 < "5");
+
+//Igual que
+
+console.log(5 >= "5");
+console.log(5 <= "5");
